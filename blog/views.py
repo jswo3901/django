875bd7974fe2e83ae3fc6772c1d587e380b4a170
jswo3901 <jswo3901@gmail.com>
2018-02-04 22:36:11 +0900

    templates to display posts from datebase.

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.utils import timezone
+from .models import Post
+#마침표(.)는 현재 디렉토리에 있는 파일
+#Post모델에서 블로그 글을 가져오기 위해서는 쿼리셋(QuerySet)이 필요하다.
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
